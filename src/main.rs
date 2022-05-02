7c5107e87ef5d19ab119b9aecaa64edb52c95e1b
Mark Oborne <markoborne1@gmail.com>
2022-05-02 16:11:08 +0100

    chore: use clap for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,21 @@
 mod prefix;
+use clap::Parser;
 use std::io::{self, Read};
-extern crate getopts;
-use getopts::Options;
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!(
-        "Usage: {} [options]
-        Pretty prints fix messages in a more human readable manner
-        Pipe other commands into or out of prefix to use",
-        program
-    );
-    print!("{}", opts.usage(&brief));
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// set delimiter string to print after each FIX field
+    #[clap(short, long, default_value = "\n")]
+    delimiter: String,
+
+    /// translate common FIX values (for Side: 1 -> Buy)
+    #[clap(short, long)]
+    value: bool,
 }
 
 fn main() {
-    let argv: Vec<String> = std::env::args().collect();
-    let program = argv[0].clone();
-
-    let mut opts = Options::new();
-    opts.optopt(
-        "d",
-        "delimiter",
-        "set delimiter string to print after each FIX fields",
-        "STRING",
-    );
-    opts.optflag(
-        "v",
-        "value",
-        "translate common FIX values (for Side: 1 -> Buy)",
-    );
-    opts.optflag("h", "help", "print this help menu");
-
-    let matches = match opts.parse(&argv[1..]) {
-        Ok(m) => m,
-        Err(f) => std::panic::panic_any(f.to_string()),
-    };
-
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return;
-    }
-
-    let mut delimiter = String::from("\n");
-    if matches.opt_present("d") {
-        delimiter = matches.opt_str("d").expect("bad delimiter string");
-    }
-
-    let mut value_flag = false;
-    if matches.opt_present("v") {
-        value_flag = true;
-    }
+    let args = Args::parse();
 
     // stdin is used to allow piping with other commands
     let mut stdin = io::stdin();
@@ -58,7 +24,7 @@ fn main() {
         .read_to_string(&mut input)
         .expect("Could not read input");
 
-    if let Err(x) = prefix::run(input, value_flag, delimiter) {
+    if let Err(x) = prefix::run(input, args.value, args.delimiter) {
         eprintln!("{}", x)
     }
 }
