e87d63ee65ca774d24b5c54a4c0408e95d858124
Mark Oborne <markoborne1@gmail.com>
2022-06-03 15:47:53 +0100

    feat: added generated manpage and completion files

---
+++ completion/prefix.fish
@@ -0,0 +1,4 @@
+complete -c prefix -s d -l delimiter -d 'Set delimiter string to print after each FIX field' -r
+complete -c prefix -s h -l help -d 'Print help information'
+complete -c prefix -s V -l version -d 'Print version information'
+complete -c prefix -s v -l value -d 'Translate common FIX values (for Side: 1 -> Buy)'
