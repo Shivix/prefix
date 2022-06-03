3d4603f68fe2442ded7be6734dbf4f151d2dff51
Mark Oborne <markoborne1@gmail.com>
2022-06-03 15:42:51 +0100

    feat: generate manpage
    Now generates a manpage during the build also.

--- build.rs
+++ build.rs
@@ -6,16 +6,23 @@ use Shell::*;
 include!("src/command.rs");
 
 fn main() -> Result<(), Error> {
-    let outdir = match std::env::var_os("OUT_DIR") {
+    let out_dir = match std::env::var_os("OUT_DIR") {
         None => return Ok(()),
-        Some(outdir) => outdir,
+        Some(out_dir) => std::path::PathBuf::from(out_dir),
     };
     let mut cmd = make_command();
 
     for shell in [Bash, Fish, PowerShell, Zsh] {
-        let path = generate_to(shell, &mut cmd, "prefix", &outdir)?;
-        println!("cargo:warning=completion file is generated: {:?}", path);
+        let completion_path = generate_to(shell, &mut cmd, "prefix", &out_dir)?;
+        println!("cargo:warning=completion file is generated: {:?}", completion_path);
     }
+
+    let man = clap_mangen::Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+    let man_path = out_dir.join("prefix.1");
+    std::fs::write(&man_path, buffer)?;
+    println!("cargo:warning=completion file is generated: {:?}", man_path);
 
     Ok(())
 }
