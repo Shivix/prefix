e65886a7c2d3950e4b340a7f1104365d98b1b73e
Mark Oborne <markoborne1@gmail.com>
2024-08-04 13:36:29 +0100

    chore: update to v1.1.2
    Sets version to 1.1.2 and updates completion and man files

--- completion/prefix.bash
+++ completion/prefix.bash
@@ -19,7 +19,7 @@ _prefix() {
 
     case "${cmd}" in
         prefix)
-            opts="-c -d -s -S -t -v -h -V --color --delimiter --strip --summary --tag --value --help --version [message]..."
+            opts="-c -d -o -s -S -t -v -h -V --color --delimiter --only-fix --strip --summary --tag --value --help --version [message]..."
             if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
--- completion/prefix.fish
+++ completion/prefix.fish
@@ -1,8 +1,9 @@
 complete -c prefix -s c -l color -d 'Adds colour to the delimiter and = in for FIX fields, auto will colour only when printing directly into a tty' -r -f -a "{always\t'',auto\t'',never\t''}"
 complete -c prefix -s d -l delimiter -d 'Set delimiter string to print after each FIX field' -r
 complete -c prefix -s S -l summary -d 'Summarise each fix message based on an optional template' -r
+complete -c prefix -s o -l only-fix -d 'Only print FIX messages'
 complete -c prefix -s s -l strip -d 'Strip the whitespace around the = in each field'
-complete -c prefix -s t -l tag -d 'Translate all numbers to tag names whether part of a message or not'
+complete -c prefix -s t -l tag -d 'Translate tag numbers on non FIX message lines'
 complete -c prefix -s v -l value -d 'Translate the values of some tags (for Side: 1 -> Buy)'
 complete -c prefix -s h -l help -d 'Print help'
 complete -c prefix -s V -l version -d 'Print version'
