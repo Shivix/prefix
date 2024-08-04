078faaf49a44a1ba99e76ab6aeaf719cb17f3c90
Mark Oborne <markoborne1@gmail.com>
2024-08-04 15:06:07 +0100

    chore: Improve completion and man generation
    The tracked completion and man files will be updated on build
    Adds a man file to fix the newline issue with the man page.
    
    Also updates test.sh to test --only-fix

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io;
 fn main() {
     let matches = command::make_command().get_matches();
 
-    let fix_message = match matches.get_many::<String>("message") {
+    let fix_message: Vec<String> = match matches.get_many::<String>("message") {
         Some(msg) => msg.map(|elem| elem.to_owned()).collect(),
         None => io::stdin().lines().map(|line| line.unwrap()).collect(),
     };
