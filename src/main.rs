ca3dad9f51adb4839e2fa26fe52d8158bd5a9586
Mark Oborne <markoborne1@gmail.com>
2025-07-19 20:15:23 +0900

    chore: update to version 1.2.0
    Generates updated completion and man pages
    
    Runs cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,28 @@ fn main() {
         let n_msgs = msgs.len();
         for (i, msg) in msgs.enumerate() {
             let is_last_line = i + 1 == n_msgs;
-            prefix::run(msg, is_last_line, &msg_regex, &tag_regex, &summary_regexes, &flags);
+            prefix::run(
+                msg,
+                is_last_line,
+                &msg_regex,
+                &tag_regex,
+                &summary_regexes,
+                &flags,
+            );
         }
     } else {
         let mut lines = io::stdin().lines().peekable();
         while let Some(line) = lines.next() {
             let line = line.unwrap();
             let is_last_line = lines.peek().is_none();
-            prefix::run(&line, is_last_line, &msg_regex, &tag_regex, &summary_regexes, &flags);
+            prefix::run(
+                &line,
+                is_last_line,
+                &msg_regex,
+                &tag_regex,
+                &summary_regexes,
+                &flags,
+            );
         }
     }
-
 }
